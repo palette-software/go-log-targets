6ac53c750138262afdecf579698f568a71ea2381
Peter Bastyi <peterb@starschema.com>
2020-06-09 15:11:52 +0200

    Add timed logger

---
+++ timed_logger.go
@@ -0,0 +1,32 @@
+package logging
+
+import (
+	"fmt"
+	"time"
+)
+
+type TimedLogger struct {
+	level   LogLevel
+	message string
+	start   time.Time
+}
+
+func NewTimedLogger(level LogLevel, format string, v ...interface{}) *TimedLogger {
+	message := fmt.Sprintf(format, v...)
+	timedLogger := TimedLogger{
+		level:   level,
+		message: message,
+		start:   time.Now(),
+	}
+	Writef(level, message+" - START")
+	return &timedLogger
+}
+
+func (tl *TimedLogger) LogEnd() {
+	tl.end = time.Now()
+	Writef(tl.level, fmt.Sprintf("%v - END  (elapsed %v ms)", tl.message, getElapsedTimeInMilliseconds(tl.start)))
+}
+
+func getElapsedTimeInMilliseconds(startTime time.Time) int64 {
+	return (time.Since(startTime).Nanoseconds()) / 1e6
+}
